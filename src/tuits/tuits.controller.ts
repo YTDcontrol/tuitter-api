ac3e2e5c73aea0884d93754c3bc68e8e0d093a9b
Carlos Adrian Sosa <adriansosa105@gmail.com>
2022-05-04 15:38:08 -0300

    Hardcoded services added.

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,31 +1,45 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Tuit } from './tuit.entity';
+import { TuitsService } from './tuits.service';
 
 @Controller('tuits')
 export class TuitsController {
+  constructor(private readonly tuitService: TuitsService) {}
+
   @Get()
-  getTuits(@Query() filterQuery): string {
+  getTuits(@Query() filterQuery): Tuit[] {
     const { searchTerm, orderBy } = filterQuery;
-    return `All ${searchTerm} tuits ordered by${orderBy}`;
+    return this.tuitService.getTuits();
   }
 
   @Get(':id')
-  getTuit(@Param('id') id: string) {
-    return `Your tuit is ${id}`;
+  getTuit(@Param('id') id: string): Tuit {
+    return this.tuitService.getTuit(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  createTuit(@Body('message') message: string) {
-    return `Your tuit wass: ${message}`;
+  createTuit(@Body('message') message: string): void {
+    return this.tuitService.createTuit(message);
   }
 
   @Patch(':id')
-  updateTuit(@Param('id') id: string, @Body('message') message: string) {
-    return `Your tuit ${id} has been updated with: ${message}`;
+  updateTuit(@Param('id') id: string, @Body('message') tuit): Tuit {
+    return this.tuitService.updateTuit(id, tuit);
   }
 
   @Delete(':id')
-  deleteTuit(@Param('id') id: string) {
-    return `Your tuit ${id} has been deleted`;
+  removeTuit(@Param('id') id: string): void {
+    return this.tuitService.removeTuit(id);
   }
 }
