c857a3a34115891a77ce146e643950882a4bb22b
Carlos Adrián Sosa <adriansosa105@gmail.com>
2022-05-13 17:20:54 -0300

    Integration with databse added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TuitsModule } from './modules/tuits/tuits.module';
 
 @Module({
-  imports: [TuitsModule],
+  imports: [
+    TuitsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'ong',
+    })
+  ],
 })
 export class AppModule {}
